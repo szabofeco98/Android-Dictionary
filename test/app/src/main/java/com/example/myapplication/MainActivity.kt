a13dceb34f1cc57b57dff251a42afb3ac78f7058
szabofeco98 <szabofeco98@gmail.com>
2019-12-08 22:10:16 +0100

    Roomdatabase+uj activity+livedata

--- test/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ test/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,27 +14,22 @@ import com.example.myapplication.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityMainBinding
-
-    private val myName: MyName = MyName()
 
     private lateinit var navController: NavController
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
 
         navController = Navigation.findNavController(this, R.id.myNavHostFragment)
 
+
         bottom_nav.setupWithNavController(navController)
 
         NavigationUI.setupActionBarWithNavController(this, navController)
-
-
-
-
 
     }
 
@@ -43,24 +38,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-     fun setNickName(view :View) {
-         /*
-         binding. apply {
-
-             myName?.nickname =  input.text.toString()
-             invalidateAll()
-             input.visibility = View.GONE
-             done.visibility = View.GONE
-             output.visibility = View.VISIBLE
-
-
-         }
-
-         // Hide the keyboard
-         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-         imm.hideSoftInputFromWindow(view.windowToken, 0)
-
-     */
-     }
 
 }
